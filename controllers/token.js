096677a29e428e74e8232801624092bf15464d00
Abhijith C S <abhijithcs@tekion.com>
2022-05-01 11:17:36 +0530

    Modal UI - added

--- controllers/token.js
+++ controllers/token.js
@@ -42,40 +42,43 @@ angular.module('TokenApp', ['ngCookies'])
       $scope.filterTitle = 'Today\'s Generated Tokens';
       $scope.isMoreLeft = true;
 
+
       //Default Results : Generated tokens of the day
-      var today = new Date();
-      var dd = today.getDate();
-      var mm = today.getMonth()+1;
-      var yyyy = today.getFullYear();
-      if(dd<10){ dd='0'+dd;}
-      if(mm<10){ mm='0'+mm;}
-      var today = dd+'-'+mm+'-'+yyyy;
+      $scope.initTokens = function() {
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth()+1;
+            var yyyy = today.getFullYear();
+            if(dd<10){ dd='0'+dd;}
+            if(mm<10){ mm='0'+mm;}
+            var today = dd+'-'+mm+'-'+yyyy;
 
-      var data = {};
-      data.token = $cookies.get("akbarTokenManagementAppAdminToken");
-      data.status = 2;
-      data.key = today;
-      
+            var data = {};
+            data.token = $cookies.get("akbarTokenManagementAppAdminToken");
+            data.status = 2;
+            data.key = today;
+            
 
-      $('#vegaPanelBodyLoader').show(); $("body").css("cursor", "progress");
-      $http({
-        method  : 'POST',
-        url     : 'https://accelerateengine.app/client-apis/akbar/filtertokens.php',
-        data    : data,
-        headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-       })
-       .then(function(response) {
-	       $('#vegaPanelBodyLoader').hide(); $("body").css("cursor", "default");
-         if(response.data.status){
-           $scope.isMoreLeft = false; //Showing all tokens anyways.
-           $scope.isOrdersFound = true;
-           $scope.completed_orders = response.data.response;
-         }
-         else{
-           $scope.isOrdersFound = false;
-           $scope.resultMessage = "There are no tokens generated today!";
-         }
-        });
+            $('#vegaPanelBodyLoader').show(); $("body").css("cursor", "progress");
+            $http({
+              method  : 'POST',
+              url     : 'https://accelerateengine.app/client-apis/akbar/filtertokens.php',
+              data    : data,
+              headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+             })
+             .then(function(response) {
+      	       $('#vegaPanelBodyLoader').hide(); $("body").css("cursor", "default");
+               if(response.data.status){
+                 $scope.isMoreLeft = false; //Showing all tokens anyways.
+                 $scope.isOrdersFound = true;
+                 $scope.completed_orders = response.data.response;
+               }
+               else{
+                 $scope.isOrdersFound = false;
+                 $scope.resultMessage = "There are no tokens generated today!";
+               }
+              });
+      }
 
       $scope.searchByDate = function(){    
 	    $scope.searchID = "";
@@ -188,4 +191,85 @@ angular.module('TokenApp', ['ngCookies'])
         $scope.isViewingOrder = false;
       }
 
+
+
+
+
+
+
+     //Add new token
+     $scope.nullNewToken = function(){
+      $scope.newTokenContent = {};
+      $scope.newTokenContent.mobile = "";
+      $scope.newTokenContent.email = "";
+      $scope.newTokenContent.name = "";
+      $scope.newTokenContent.date = "";
+      $scope.newTokenContent.time = "";
+      $scope.newTokenContent.comments = "";     
+     }
+     $scope.nullNewToken();
+     
+     
+     $scope.openNewToken = function(){
+      $scope.nullNewToken();
+      $('#tokenModal').modal('show');
+     }
+
+
+
+
+
+      
+  $scope.saveNewToken = function(){
+  
+    $scope.newTokenContent.time = document.getElementById("new_time").value;
+    $scope.newTokenContent.date = document.getElementById("new_date").value;
+    $scope.newTokenContent.mode = document.getElementById("my_mode").value;
+    $scope.newTokenContent.isBirthday = document.getElementById("check_birthday").checked ? 1 : 0;
+    $scope.newTokenContent.isAnniversary = document.getElementById("check_anniversary").checked? 1: 0;
+    
+    console.log($scope.newTokenContent);
+    
+    $scope.newTokenError = "";
+    
+    if($scope.newTokenContent.name == "" || !(/^[a-zA-Z ]+$/.test($scope.newTokenContent.name))){
+      $scope.newTokenError = "Invalid Name";
+    }
+    else if($scope.newTokenContent.mobile == "" || !(/^[789]\d{9}$/.test($scope.newTokenContent.mobile))){
+      $scope.newTokenError = "Invalid Mobile Number";
+    }
+    else if($scope.newTokenContent.count == "" || $scope.newTokenContent.count == 0){
+      $scope.newTokenError = "Invalid Person Count";
+    }
+    else if($scope.newTokenContent.date == "" || $scope.newTokenContent.time == ""){
+      $scope.newTokenError = "Add Date and Time";
+    }
+    else if($scope.newTokenContent.mode == ""){
+      $scope.newTokenError = "Select Reservation Mode";
+    }
+    else{
+      $scope.newTokenError = "";
+      
+      var data = {};
+          data.details = $scope.newTokenContent;
+            data.token = $cookies.get("acceleronLunaAdminToken");
+            $http({
+              method  : 'POST',
+              url     : 'https://accelerateengine.app/client-apis/akbar/generatetoken.php',
+              data    : data,
+              headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+             })
+             .then(function(response) {
+              $('#tokenModal').modal('hide');
+              if(response.data.status){    
+                $scope.initTokens();
+              }
+              else{
+                alert(response.data.error);
+              }
+             });  
+    }
+  
+  }
+  
 });
